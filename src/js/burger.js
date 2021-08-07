fe91e38fbbb3d1adf7a8011ac541e935aeda5a77
IsakovMaxim <85799786+IsakovMaxim@users.noreply.github.com>
2021-08-07 17:31:20 +0500

    Убран скролл при открытии меню на моб.версии

--- src/js/burger.js
+++ src/js/burger.js
@@ -4,8 +4,35 @@
 	const menuCloseBtn = document.querySelector('.header__close-burger');
 	menuBtn.addEventListener("click", () => {
         menu.classList.toggle("is-open");
+		disabledScroll();
 	});
 
 	menuCloseBtn.addEventListener("click", () => {
         menu.classList.toggle("is-open");
+		enabledScroll();
 	});
+
+	// НАСТРЙОКА СКРОЛЛА ПРИ ОТКРЫТИИ/ЗАКРЫТИИ МЕНЮ НА МОБИЛЬНОЙ ВЕРСИИ
+
+	const disabledScroll = () => {
+		const widthScroll = window.innerWidth - document.body.offsetWidth;
+	
+		document.body.dbScrollY = window.scrollY;
+	   
+		document.body.style.cssText = `
+			position: fixed;
+			top: ${-window.scrollY}px;
+			left: 0;
+			width: 100%;
+			height: 100vh;
+			overflow: hidden;
+			padding-right: ${widthScroll}px;
+		`;
+	};
+	
+	const enabledScroll = () => {
+		document.body.style.cssText = '';
+		window.scroll({
+			top: document.body.dbScrollY,   
+		})
+	};
